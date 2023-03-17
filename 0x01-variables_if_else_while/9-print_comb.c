1dd4f20eb4a839ea6db6f2ec301ce089d5eb7f7e
Patrick Kipsum <pat.koech01@gmail.com>
2023-03-17 09:26:09 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: combinations of single-digit numbers
+ * Retutn: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 48; i <= 48; i++)
+	{
+		putchar((char)i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
