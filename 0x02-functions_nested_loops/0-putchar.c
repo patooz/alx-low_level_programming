cc6a0c6f0baed40243e0b36022b509ec68e89a58
Patrick Kipsum <pat.koech01@gmail.com>
2023-03-21 02:38:43 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print _putchar.c
+ * Return: 0 (success)
+ */
+int main(void)
+{
+        putchar('_');
+        putchar('p');
+        putchar('u');
+        putchar('t');
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+        putchar('\n');
+        return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-putchar.c - a C program that prints _putchar, followed by a new line.
