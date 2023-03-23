bf88f27d89837da1b06f5f435aa75685bf37abb6
Patrick Kipsum <pat.koech01@gmail.com>
2023-03-23 05:36:55 -0700

    int i, j;

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: 0 to 14 10 times
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++ )
+		{
+			if (j < 10)
+				_putchar('0' + 1);
+			_putchar('0' + (j % 10));
+		}
+	}
+	_putchar('\n');
+}
