18ed538ad90c061808d6a3862464fab9906ca83c
Patrick Kipsum <pat.koech01@gmail.com>
2023-03-30 00:19:29 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,11 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (*s1 - *s2);
 		}
-		if (*s1 != '\0')
-			return (*s1);
-		else if (*s2 != '\0')
-			return (*s2);
 	}
+	if (*s1 != '\0')
+		return (*s1);
+	else if (*s2 != '\0')
+		return (*s2);
+	
 	return (0);
 }
