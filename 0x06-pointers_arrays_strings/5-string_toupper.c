d7e23ccfa7abad715bc07b22db2c83589715785c
Patrick Kipsum <pat.koech01@gmail.com>
2023-03-30 02:07:36 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,15 @@
  */
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '0'; i++)
+	while (s[i])
 	{
 		if (s[i] >= 97 && s[i] <= 122)
+		{
 			s[i] -= 32;
+		}
+		i++;
 	}
-	return (s)
+	return (s);
 }
