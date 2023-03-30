74b0497d77f4a7fc747cceeecdf78f219527e51d
Patrick Kipsum <pat.koech01@gmail.com>
2023-03-30 04:36:23 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		j++
+		j++;
 		i++;
 	}
 	return (dest);
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
