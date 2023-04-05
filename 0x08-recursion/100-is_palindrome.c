30315e3d3e06991b30e973549f96dc6d8f2986c8
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-05 13:04:06 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen - length of thr string
+ * @s: length of the string
+ * Return: length of the string
+ */
+int  _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+
+}
+/**
+ * _check_pal - check if the string is a palindrome
+ * @s: string
+ * @i: left index
+ * @j: right index
+ * Return: 1 if palindrome else 0
+ */
+int _check_pal(char *s, int i, int j)
+{
+	if (i >= j)
+		return (i);
+	if (s[i] != s[j])
+		return (0);
+	return (_check_pal(s, i + 1, j - 1));
+}
+/**
+ * is_palindrome - if a string is a palindrome or not
+ * @s: string
+ * Return: 1 if palindrome else 0
+ */
+int is_palindrome(char *s)
+{
+	return (_check_pal(s, 0,  _strlen(s) - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,4 @@
 2-strlen_recursion.c - A C function that returns the length of a string.
 4-pow_recursion.c - A C function that returns the value of x raised to the power of y.
 5-sqrt_recursion.c - A C function that returns the natural square root of a number.
+6-is_prime_number.c - A C function that returns 1 if the input integer is a prime number, otherwise return 0.
