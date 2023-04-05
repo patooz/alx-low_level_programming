9f5d2fbe75d9eab2bbd89acb48c6b3588d2dc3b3
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-05 08:55:14 -0700

    You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a given number
+ * @n: integer to be factored
+ * Return: -1 if n is less than 0
+ * factorial of 0 is 1
+ */
+int factorial(int n)
+{
+	if (n > 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
