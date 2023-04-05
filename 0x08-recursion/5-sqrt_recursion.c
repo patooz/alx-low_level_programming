7692e8962a99f4305c43c1a76898d1d5268fd9f5
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-05 11:28:57 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion- finds the square root of a number
+ * @n: integer
+ * Return: square root of a number,
+ * if n has no natural number, returns -1
+ */
+int _sqrt_recursion(int n);
+{
+	if (x * x > n)
+		return (-1);
+	if (x * x == n)
+		return (x);
+	return (_sqrt_recursion(n, x + 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,3 +2,4 @@
 1-print_rev_recursion.c - A C function that prints a string in reverse.
 2-strlen_recursion.c - A C function that returns the length of a string.
 4-pow_recursion.c - A C function that returns the value of x raised to the power of y.
+5-sqrt_recursion.c - A C function that returns the natural square root of a number.
