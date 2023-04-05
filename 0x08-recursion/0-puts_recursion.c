9915fdf1a68c4dba2acccba88f901bc6206b2780
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-05 03:35:24 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer of character to be printed
+ * Return: printef string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s++);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+0-puts_recursion.c - A C function that prints a string, followed by a new line.
