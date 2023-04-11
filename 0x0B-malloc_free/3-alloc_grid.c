50f3ed84c438c0140f2ae512f7a1acbc7307d9c3
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-11 13:16:30 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.k"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: first integer
+ * @height: second integer
+ * Return: NULL on failure
+ * NULL if width or height is 0 or negative
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = (int **)malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; ++i)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; ++j)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; ++j)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 0-create_array.c - A C function that creates an array of chars, and initializes it with a specific char.
 1-strdup.c - A C function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
 2-str_concat.c - A C function that concatenates two strings.
+3-alloc_grid.c - A C function that returns a pointer to a 2 dimensional array of integers.
