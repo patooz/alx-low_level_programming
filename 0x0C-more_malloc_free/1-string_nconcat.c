1598966ae12756b7966ee948e84df5eea0c684d6
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-13 06:42:57 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; ++i)
 		++k1;
 	k2 = k1 + n;
-	m = malloc(sizeof(char) * (k2 + 1))
-		if (!m)
-			return (NULL);
+	m = malloc(sizeof(char) * (k2 + 1));
+	if (!m)
+		return (NULL);
 	for (i = 0; i < k1; ++i)
 		m[i] = s1[i];
 	for (j = 0; j < n; ++j, ++i)
