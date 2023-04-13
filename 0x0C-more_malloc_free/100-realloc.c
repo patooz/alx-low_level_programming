48a40827f8651dae486de80f0a4399f0932a4ae1
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-13 07:50:58 -0700

    _realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: sixe in bytesold_size
+ * @new_size: new size, in bytes of the new memory block
+ * Return: 0
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+			printf(" ");
+		if (!(i % 10) && i)
+			printf("\n");
+		printf("0x%2x", buffer[i]);
+		i++
+	}
+	printf("\n");
+}
+/**
+ * main - check code
+ * Return: 0
+ */
+int main(void)
+{
+	char *s;
+	int i;
+
+	s = malloc(sizeof(char) * 10);
+	s = _realloc(s, sizeof(char) * 10, sizeof(char) * 98);
+	i = 0;
+	while (i < 98)
+		s[i++] = 98;
+	sumple_print_buffer(s, 98);
+	free(s);
+	return (0);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -2,3 +2,4 @@
 1-string_nconcat.c - A C function that concatenates two strings.
 2-calloc.c - A C function that allocates memory for an array, using malloc.
 3-array_range.c - A C function that creates an array of integers.
+100-realloc.c - A C function that reallocates a memory block using malloc and free
