e8fc91b1e1826fc3458276f510098643b48ee390
Patrick Kipsum <pat.koech01@gmail.com>
2023-04-17 03:04:19 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,18 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- *my_dog - describes thr dog
- *@name: name of the dog
- *@age: age of the dog
- *@owner: owner of the dog
- *@s: pointer to the struct dog
+ * struct dog - describes the dog
+ * @name: name of thr dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
-void my_dog(struct dog *s, char *name, float age, char *owner)
+struct dog
 {
-	if (s != 0)
-	{
-		s->name = name;
-		s->age = age;
-		s->owner = owner;
-	}
-
+	char *name;
+	float age;
+	char *owner;
 }
+
+
+#endif /* DOG_H */
