5cd568d4271edfecd355d51cd5a8a9c1478080c3
Patrick Kipsum <pat.koech01@gmail.com>
2023-05-01 23:55:48 -0700

    7. Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (a = 0; a < index; a++)
 	{
 		if (head != NULL)
-			head = head->next
+			head = head->next;
 	}
 	return (head);
 }
