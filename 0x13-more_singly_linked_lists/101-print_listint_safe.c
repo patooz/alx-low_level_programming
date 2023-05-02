b93ef6bcd6b896e0582a8efafd0900c709a411c0
Patrick Kipsum <pat.koech01@gmail.com>
2023-05-02 03:51:31 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,45 @@
 #include "lists.h"
 
 /**
+ *uni_node - unique nodes
+ *@head: pointer to the head pointer
+ *Return: 0 if list is not looped
+ */
+size_t uni_node(const listint_t *head)
+{
+	const listint_t *x, *y;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	x = head->next;
+	y = (head->next)->next;
+	while (y)
+	{
+		if (x == y)
+		{
+			x = head;
+			while (x != y)
+			{
+				nodes++;
+				x = x->next;
+				y = y->next;
+			}
+			x = x->next;
+			while (x != y)
+			{
+				nodes++;
+				x = x->next;
+			}
+			return (nodes);
+		}
+		x = x->next;
+		y = (y->next)->next
+	}
+	return (0);
+}
+
+/**
  * print_listint_safe - prints a linked list with a loop
  * @head: pointet to the head node
  *
@@ -8,20 +47,25 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *k;
-	size_t i = 0;
+	size_t nodes, i = 0;
 
-	k = head;
-	while (head != NULL)
+	nodes = uni_node(head);
+	if (nodes == 0)
 	{
-		i++;
-		printf("[%p] %d\n", (*void *)k, k->n);
-		k = k->next;
-		if (k >= head && k != NULL)
+		for (; head != NULL; nodes++)
 		{
-			printf("-> [%p] %d\n", (void *)k, k->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
-	return (i);
+	else
+	{
+		for (i = 0; i < nodes; i++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (nodes);
 }
