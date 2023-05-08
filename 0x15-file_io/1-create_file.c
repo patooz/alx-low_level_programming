292850314733ecc076ac8ad79cac5d93264b4316
Patrick Kipsum <pat.koech01@gmail.com>
2023-05-08 10:03:59 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 	int i = 0, j;
 
 	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
 		text_content = "";
 	while (text_content[i] != '\0')
 		i++;
