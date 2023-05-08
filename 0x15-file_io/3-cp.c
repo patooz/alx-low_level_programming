730404e248d9f78ab1b1339268a23511a4bee5bf
Patrick Kipsum <pat.koech01@gmail.com>
2023-05-08 11:10:49 -0700

    3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argv: argument string in an array
+ * @argc: argument count
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int file_to, file_from;
+	int k = 1024, l = 0;
+	char m[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | A_IWUSR
+			| S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from), exit(99);
+	}
+	while (k == 1024)
+	{
+		k = read(file_from, m, 1024);
+		if (k == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		l = write(file_to, m, k)
+			if (l < k)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2], exit(99));
+	}
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_from, exit(100));
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_to, exit(100));
+	return (0);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -2,3 +2,4 @@
 1-create_file.c - A C function that creates a file.
 A C function that appends text at the end of a file.
 2-append_text_to_file.c - A C function that appends text at the end of a file.
+3-cp.c - A C program that copies the content of a file to another file.
