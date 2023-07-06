406a288c60b610bd7b7381f327f3f7e06c77c297
Patrick Kipsum <pat.koech01@gmail.com>
2023-07-06 03:45:10 -0700

    3. 11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a value of a bit to 1
+ * @index: index starting from 0
+ * @n: pointer to the index
+ * Return: 1 if it works, -1 if error occurs
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 63)
+		return (-1);
+	i = 1 << index;
+	*n = (*n | 1);
+	return (1);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,3 +1,4 @@
 0-binary_to_uint.c - A C function that converts a binary number to an unsigned int.
 1-print_binary.c - A C function that prints the binary representation of a number.
 2-get_bit.c - A C function that returns the value of a bit at a given index.
+3-set_bit.c - A C function that sets the value of a bit to 1 at a given index.
