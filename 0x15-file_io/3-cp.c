6a0d9c6b3c85c5f2923274367e7f0c3e09ee1cf6
Patrick Kipsum <pat.koech01@gmail.com>
2023-07-10 07:57:04 -0700

    fixe n an erro in 3-cp.c file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	char m[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
