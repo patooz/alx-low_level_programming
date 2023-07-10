e2a3d649f06d9065e387e3ccf6d0f58114da25e7
Patrick Kipsum <pat.koech01@gmail.com>
2023-07-10 07:44:18 -0700

    fixed an error in 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	j = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (j == -1)
-		return (-1)
+		return (-1);
 	write(j, text_content, i);
 	return (1);
 }
