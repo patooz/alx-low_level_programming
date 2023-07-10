e7432bd9e634978efb696be23c5623091ef26156
Patrick Kipsum <pat.koech01@gmail.com>
2023-07-10 07:24:27 -0700

    1. Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,7 @@
 int _putchar (char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 
 void _check(unsigned char *e_ident);
 void _magic(unsigned char *e_ident);
