cb1deeb52a8781e489a7145d05aa05236fc51638
Patrick Kipsum <pat.koech01@gmail.com>
2023-08-17 02:39:38 -0700

    fixed an error

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *i;
 	dlistint_t *j;
 
-	i = maloc(sizeof(dlistint_t));
+	i = malloc(sizeof(dlistint_t));
 	if (i == NULL)
 		return (NULL);
 	i->n = n;
