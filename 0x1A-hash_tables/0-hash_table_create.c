671663576d8fca31c6292fb670e890ba5ab61f1a
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-09 22:47:50 +0300

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates tht hash table
+ * @size: size of the array
+ * Return: the size of the array
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *i;
+	unsigned long int j;
+
+	i = malloc(sizeof(hash_table_t));
+	if (i == NULL)
+		return (NULL);
+	i->size = size;
+	i->array = malloc(sizeof(hash_node_t *) * size);
+	if (i->array == NULL)
+		return (NULL);
+	for (j = 0; j < size; j++)
+		i->array[j] = NULL;
+	return (i);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+0-hash_table_create.c - a function that creates a hash table.
