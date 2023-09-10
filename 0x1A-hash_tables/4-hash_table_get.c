bfa7daaf774ff291fce2de0fdd4fb9ac92c5132c
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-10 18:41:58 +0300

    fixed an error

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	j = key_index((const unsigned char *)key, ht->size);
 	if (j >= ht->size)
 		return (NULL);
-	node = ht->array[j];
+	i = ht->array[j];
 	while (i && strcmp(i->key, key) != 0)
 		i = i->next;
 	return ((i == NULL) ? NULL : i->value);
