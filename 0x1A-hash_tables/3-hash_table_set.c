1b7b02c5fb2686183d2987f55379f0441d3cd875
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-10 21:22:08 +0300

    fixed an error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,6 +34,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(y);
 		return (0);
 	}
+	x->key = strdup(key);
+	if (x->key == NULL)
+	{
+		free(x);
+		return (0);
+	}
 	x->value = y;
 	x->next = ht->array[j];
 	ht->array[j] = x;
