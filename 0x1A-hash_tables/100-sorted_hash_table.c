c424e4d1aab2548b42d8af7ce1687b3a52f64d23
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-11 16:15:24 +0300

    fixed issues

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,12 +1,5 @@
 #include "hash_tables.h"
 
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
-
 /**
  * shash_table_create - makes a sorted hash table
  * @size: size of the hash table
@@ -93,16 +86,16 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		k = ht->shead;
-		while (k->snext != NULL && strcmp(k->snext->key, key) < 0)
-			k = k->snext;
-		i->sprev = k;
-		i->snext = k->next;
-		if (k->snext == NULL)
+		j = ht->shead;
+		while (j->snext != NULL && strcmp(j->snext->key, key) < 0)
+			j = j->snext;
+		i->sprev = j;
+		i->snext = j->next;
+		if (j->snext == NULL)
 			ht->stail = i;
 		else
-			k->snext->sprev = i;
-		k->snext = i;
+			j->snext->sprev = i;
+		j->snext = i;
 	}
 	return (1);
 }
@@ -193,6 +186,6 @@ void shash_table_delete(shash_table_t *ht)
 		free(i);
 		i = k;
 	}
-	free(head->array);
-	free(head);
+	free(h->array);
+	free(h);
 }
